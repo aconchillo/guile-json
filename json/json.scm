e710de589a544cd2f326cd81f292d11c576a2758
Aleix Conchillo Flaque <aconchillo@gmail.com>
2013-01-26 14:25:23 -0800

    fix number parsing inside objects and arrays

--- json/json.scm
+++ json/json.scm
@@ -20,7 +20,7 @@
   (let loop ((c (peek-char port)) (s ""))
     (case c
       ((#\+ #\-)
-       (let ((ch (read-char port))) ; current char
+       (let ((ch (read-char port)))
          (string-append s (string ch))))
       (else s))))
 
@@ -30,74 +30,98 @@
   (let loop ((c (peek-char port)) (s ""))
     (case c
       ((#\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9)
-       (let ((ch (read-char port))) ; current char
-         (loop (peek-char port)     ; next char
+       (let ((ch (read-char port)))
+         (loop (peek-char port)
                (string-append s (string ch)))))
       (else s))))
 
 (define (read-exp-part port)
-  (let loop ((c (peek-char port)) (s ""))
-    ;; Return string when reaching enf of string.
-    (if (eof-object? c)
-        s
-        (case c
-          ((#\e #\E)
-           (let ((ch (read-char port)) ; current char
-                 (sign (read-sign port))
-                 (digits (read-digits port)))
-             ;; If we don't have sign or digits, we have an invalid
-             ;; number.
-             (if (not (and (string-null? sign)
-                           (string-null? digits)))
-                 (string-append s (string ch) sign digits)
-                 #f)))
-          ;; If we have a character different than e or E, we have an
-          ;; invalid number.
-          (else #f)))))
+  (let ((c (peek-char port)) (s ""))
+    (pk "hola" s)
+    (cond
+     ;; Stop parsing if end of string found.
+     ((eof-object? c) s)
+     ;; Stop parsing if whitespace found.
+     ((char-whitespace? c) s)
+     (else
+      (case c
+        ;; We might be in an array or object, so stop here too.
+        ((#\, #\] #\}) s)
+        ;; We might have the exponential part
+        ((#\e #\E)
+         (let ((ch (read-char port)) ; current char
+               (sign (read-sign port))
+               (digits (read-digits port)))
+           ;; If we don't have sign or digits, we have an invalid
+           ;; number.
+           (if (not (and (string-null? sign)
+                         (string-null? digits)))
+               (string-append s (string ch) sign digits)
+               #f)))
+        ;; If we have a character different than e or E, we have an
+        ;; invalid number.
+        (else #f))))))
 
 (define (read-real-part port)
-  (let loop ((c (peek-char port)) (s ""))
-    ;; Return string when reaching enf of string.
-    (if (eof-object? c)
-        s
-        (case c
-          ((#\.)
-           (let ((ch (read-char port)) ; current char
-                 (digits (read-digits port)))
-             ;; If we have digits read the exponential part, otherwise
-             ;; we have an invalid number.
-             (if (not (string-null? digits))
-                 (let ((exp (read-exp-part port)))
-                   (if exp
-                       (string-append s (string ch) digits exp)
-                       #f))
-                 #f)))
-          ;; If we have a character different than ., we might continue
-          ;; processing.
-          (else #f)))))
+  (let ((c (peek-char port)) (s ""))
+    (pk "hola" s)
+    (cond
+     ;; Return string when reaching enf of string.
+     ((eof-object? c) s)
+     ;; Stop parsing if whitespace found.
+     ((char-whitespace? c) s)
+     (else
+      (case c
+        ;; We might be in an array or object, so stop here too.
+        ((#\, #\] #\}) s)
+        ;; If we read . we might have a real number
+        ((#\.)
+         (let ((ch (read-char port))
+               (digits (read-digits port)))
+           ;; If we have digits, try to read the exponential part,
+           ;; otherwise we have an invalid number.
+           (cond
+            ((not (string-null? digits))
+             (let ((exp (read-exp-part port)))
+               (cond
+                (exp (string-append s (string ch) digits exp))
+                (else #f))))
+            (else #f))))
+        ;; If we have a character different than . we might continue
+        ;; processing.
+        (else #f))))))
 
 (define (read-number port)
   (let loop ((c (peek-char port)) (s ""))
-    (case c
-      ((#\-)
-       (let ((ch (read-char port))) ; current char
-         (loop (peek-char port)     ; next char
-               (string-append s (string ch)))))
-      ((#\0)
-       (let ((ch (read-char port)))
-         (string-append s
-                        (string ch)
-                        (or (read-real-part port)
-                            (throw 'json-invalid-number)))))
-      ((#\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9)
-       (let ((ch (read-char port))) ; current char
-         (string-append s
-                        (string ch)
-                        (read-digits port)
-                        (or (read-real-part port)
-                            (read-exp-part port)
-                            (throw 'json-invalid-number)))))
-      (else (throw 'json-invalid-number)))))
+    (pk s)
+    (cond
+     ;; Return string when reaching enf of string.
+     ((eof-object? c) s)
+     ;; Stop parsing if whitespace found.
+     ((char-whitespace? c) s)
+     (else
+      (case c
+        ;; We might be in an array or object, so stop here too.
+        ((#\, #\] #\}) s)
+        ((#\-)
+         (let ((ch (read-char port)))
+           (loop (peek-char port)
+                 (string-append s (string ch)))))
+        ((#\0)
+         (let ((ch (read-char port)))
+           (string-append s
+                          (string ch)
+                          (or (read-real-part port)
+                              (throw 'json-invalid-number)))))
+        ((#\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9)
+         (let ((ch (read-char port)))
+           (string-append s
+                          (string ch)
+                          (read-digits port)
+                          (or (read-real-part port)
+                              (read-exp-part port)
+                              (throw 'json-invalid-number)))))
+        (else (throw 'json-invalid-number)))))))
 
 ;;
 ;; String parsing helpers
@@ -176,21 +200,26 @@
        (throw 'json-invalid-string)))))
 
 (define (json-read port)
-  (let skipwhitespace ((c (peek-char port)))
-    (case c
-      ((#\t) (json-read-true port))
-      ((#\f) (json-read-false port))
-      ((#\n) (json-read-null port))
-      ;; skip bracket and read array
-      ((#\[)
-       (read-char port)
-       (json-read-array port))
-      ;; skip double quote and read string
-      ((#\")
-       (read-char port)
-       (json-read-string port))
-      ;; anything else must be a number
-      (else (json-read-number port)))))
+  (let loop ((c (peek-char port)))
+    (cond
+     ((char-whitespace? c)
+      (read-char port)
+      (loop (peek-char port)))
+     (else
+      (case c
+        ((#\t) (json-read-true port))
+        ((#\f) (json-read-false port))
+        ((#\n) (json-read-null port))
+        ;; skip bracket and read array
+        ((#\[)
+         (read-char port)
+         (json-read-array port))
+        ;; skip double quote and read string
+        ((#\")
+         (read-char port)
+         (json-read-string port))
+        ;; anything else must be a number
+        (else (json-read-number port)))))))
 
 (define (json->scm s)
   (json-read (open-input-string s)))
