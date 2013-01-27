e1158b5aefb2dc71716585c9da3eb024f5cad3bb
Aleix Conchillo Flaque <aconchillo@gmail.com>
2013-01-26 23:28:03 -0800

    prepare guile-json library for installation

--- src/json.scm
+++ src/json.scm
@@ -1,5 +1,35 @@
+;;; (json) --- Guile JSON implementation.
+
+;; Copyright (C) 2013 Aleix Conchillo Flaque <aconchillo at gmail dot com>
+;;
+;; This file is part of guile-json.
+;;
+;; guile-json is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3 of
+;; the License, or (at your option) any later version.
+;;
+;; guile-json is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, contact:
+;;
+;; Free Software Foundation           Voice:  +1-617-542-5942
+;; 59 Temple Place - Suite 330        Fax:    +1-617-542-2652
+;; Boston, MA  02111-1307,  USA       gnu@gnu.org
+
+;;; Commentary:
+
+;; JSON module for Guile
+
+;;; Code:
+
 (define-module (json)
   #:use-module (ice-9 rdelim)
+  #:use-module (rnrs bytevectors)
   #:export (json->scm))
 
 ;;
@@ -290,3 +320,5 @@
 
 (define (json->scm s)
   (json-read (open-input-string s)))
+
+;;; (json) ends here
